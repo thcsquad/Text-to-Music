cd673cc47cc59615ec76527ce8548490abb04488
Tristan Crockett <thcsquad@gmail.com>
2011-07-13 01:13:42 -0400

    print out word lengths from test file, arranged into sentences

--- analyze.py
+++ analyze.py
@@ -9,3 +9,11 @@ text = f.read()
 paragraphs = text.split('\n\n')
 for i in range(0,len(paragraphs)):
 	paragraphs[i] = tokenizer.tokenize(paragraphs[i])
+	for j in range(0, len(paragraphs[i])):
+		paragraphs[i][j] = paragraphs[i][j].split(' ')
+
+length = lambda x: str(len(x))
+for paragraph in paragraphs:
+	for sentence in paragraph:
+		print " ".join(map(length, sentence))
+	print "\n"
